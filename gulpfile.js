49ff3ca2f636a174b9173e41cea86dcc51c7d8fe
Richard <richard.rabillon@live.fr>
2022-12-09 16:58:26 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp');
-var newer = require('gulp-newer');
-var imagemin = require('gulp-imagemin');
+import gulp from 'gulp';
+import newer from 'gulp-newer';
+import imagemin,  {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
 
 var imgSrc = 'src/images/**';
 var imgDest = 'dist/images';
@@ -10,10 +10,10 @@ gulp.task('images', function() {
     return gulp.src(imgSrc)
         .pipe(newer(imgDest))
         .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 75, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
+            gifsicle({interlaced: true}),
+            mozjpeg({quality: 75, progressive: true}),
+            optipng({optimizationLevel: 5}),
+            svgo({
                 plugins: [
                     {removeViewBox: true},
                     {cleanupIDs: false}
@@ -29,5 +29,3 @@ gulp.task('images', function() {
 gulp.task('watch', function() {
     gulp.watch(imgSrc, gulp.series('images'));
 });
-
-
